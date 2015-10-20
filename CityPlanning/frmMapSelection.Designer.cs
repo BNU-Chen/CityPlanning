39934da0cb549a1c3ccdff0ac20e1276e5f8f33a
Kingvey <895045872@qq.com>
2015-10-20 22:50:09 +0800

    添加图集画廊浏览选择窗口

---
+++ CityPlanning/frmMapSelection.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CityPlanning
+{
+    partial class frmMapSelection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.layoutView1 = new DevExpress.XtraGrid.Views.Layout.LayoutView();
+            this.layoutViewCard1 = new DevExpress.XtraGrid.Views.Layout.LayoutViewCard();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.layoutViewColumn1 = new DevExpress.XtraGrid.Columns.LayoutViewColumn();
+            this.layoutViewField_layoutViewColumn1 = new DevExpress.XtraGrid.Views.Layout.LayoutViewField();
+            this.btnStatusMap = new System.Windows.Forms.Button();
+            this.btnPlanMap = new System.Windows.Forms.Button();
+            this.btnAnalysisMap = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutViewCard1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutViewField_layoutViewColumn1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutView1
+            // 
+            this.layoutView1.CardMinSize = new System.Drawing.Size(280, 196);
+            this.layoutView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.LayoutViewColumn[] {
+            this.layoutViewColumn1});
+            this.layoutView1.GridControl = this.gridControl1;
+            this.layoutView1.Name = "layoutView1";
+            this.layoutView1.TemplateCard = this.layoutViewCard1;
+            // 
+            // layoutViewCard1
+            // 
+            this.layoutViewCard1.CustomizationFormText = "TemplateCard";
+            this.layoutViewCard1.ExpandButtonLocation = DevExpress.Utils.GroupElementLocation.AfterText;
+            this.layoutViewCard1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutViewField_layoutViewColumn1});
+            this.layoutViewCard1.Name = "layoutViewCard1";
+            this.layoutViewCard1.OptionsItemText.TextToControlDistance = 5;
+            this.layoutViewCard1.Text = "TemplateCard";
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridControl1.Location = new System.Drawing.Point(0, 54);
+            this.gridControl1.MainView = this.layoutView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(784, 510);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.layoutView1});
+            // 
+            // layoutViewColumn1
+            // 
+            this.layoutViewColumn1.Caption = "layoutViewColumn1";
+            this.layoutViewColumn1.LayoutViewField = this.layoutViewField_layoutViewColumn1;
+            this.layoutViewColumn1.Name = "layoutViewColumn1";
+            // 
+            // layoutViewField_layoutViewColumn1
+            // 
+            this.layoutViewField_layoutViewColumn1.EditorPreferredWidth = 10;
+            this.layoutViewField_layoutViewColumn1.Location = new System.Drawing.Point(0, 0);
+            this.layoutViewField_layoutViewColumn1.Name = "layoutViewField_layoutViewColumn1";
+            this.layoutViewField_layoutViewColumn1.Size = new System.Drawing.Size(250, 20);
+            this.layoutViewField_layoutViewColumn1.TextSize = new System.Drawing.Size(111, 14);
+            this.layoutViewField_layoutViewColumn1.TextToControlDistance = 5;
+            // 
+            // btnStatusMap
+            // 
+            this.btnStatusMap.AutoSize = true;
+            this.btnStatusMap.Font = new System.Drawing.Font("楷体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStatusMap.Location = new System.Drawing.Point(132, 9);
+            this.btnStatusMap.Name = "btnStatusMap";
+            this.btnStatusMap.Size = new System.Drawing.Size(116, 39);
+            this.btnStatusMap.TabIndex = 1;
+            this.btnStatusMap.Text = "现状图";
+            this.btnStatusMap.UseVisualStyleBackColor = true;
+            this.btnStatusMap.Click += new System.EventHandler(this.btnStatusMap_Click);
+            // 
+            // btnPlanMap
+            // 
+            this.btnPlanMap.AutoSize = true;
+            this.btnPlanMap.Font = new System.Drawing.Font("楷体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPlanMap.Location = new System.Drawing.Point(334, 9);
+            this.btnPlanMap.Name = "btnPlanMap";
+            this.btnPlanMap.Size = new System.Drawing.Size(116, 39);
+            this.btnPlanMap.TabIndex = 2;
+            this.btnPlanMap.Text = "规划图";
+            this.btnPlanMap.UseVisualStyleBackColor = true;
+            this.btnPlanMap.Click += new System.EventHandler(this.btnPlanMap_Click);
+            // 
+            // btnAnalysisMap
+            // 
+            this.btnAnalysisMap.AutoSize = true;
+            this.btnAnalysisMap.Font = new System.Drawing.Font("楷体", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnAnalysisMap.Location = new System.Drawing.Point(536, 9);
+            this.btnAnalysisMap.Name = "btnAnalysisMap";
+            this.btnAnalysisMap.Size = new System.Drawing.Size(116, 39);
+            this.btnAnalysisMap.TabIndex = 3;
+            this.btnAnalysisMap.Text = "分析图";
+            this.btnAnalysisMap.UseVisualStyleBackColor = true;
+            this.btnAnalysisMap.Click += new System.EventHandler(this.btnAnalysisMap_Click);
+            // 
+            // frmMapSelection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 564);
+            this.Controls.Add(this.btnAnalysisMap);
+            this.Controls.Add(this.btnPlanMap);
+            this.Controls.Add(this.btnStatusMap);
+            this.Controls.Add(this.gridControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMapSelection";
+            this.Load += new System.EventHandler(this.frmMapSelection_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutViewCard1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutViewField_layoutViewColumn1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.Views.Layout.LayoutView layoutView1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Layout.LayoutViewCard layoutViewCard1;
+        private DevExpress.XtraGrid.Columns.LayoutViewColumn layoutViewColumn1;
+        private DevExpress.XtraGrid.Views.Layout.LayoutViewField layoutViewField_layoutViewColumn1;
+        private System.Windows.Forms.Button btnStatusMap;
+        private System.Windows.Forms.Button btnPlanMap;
+        private System.Windows.Forms.Button btnAnalysisMap;
+
+
+    }
+}
---
+++ CityPlanning/frmMapSelection.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Layout;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraLayout;
+using DevExpress.XtraLayout.Customization;
+using DevExpress.XtraLayout.Utils;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraEditors.Controls;
+
+namespace CityPlanning
+{
+    public partial class frmMapSelection : Form
+    {
+        private string mainFloderPath;
+        private Size cardSize;
+
+        public frmMapSelection()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMapSelection_Load(object sender, EventArgs e)
+        {
+            cardSize = new Size(this.Size.Width / 2, this.Size.Height / 2);
+            mainFloderPath = @"F:\项目资料\项目-沈阳经济开发区\项目 - 沈阳经济区\图集\经济区图册正面";
+            SetGridControlView(mainFloderPath, cardSize);
+        }
+
+        private void SetGridControlView(string imageFloderPath, Size cardSize)
+        {
+            string[] filePathes = Directory.GetFiles(imageFloderPath, "*.jpg");
+            DataTable dt = CreatDatatableOfFiles(filePathes, new Size(400, 300));
+            
+            this.gridControl1.DataSource = dt;
+            this.gridControl1.ViewCollection.Clear();
+            LayoutView lView = new LayoutView(this.gridControl1);
+            this.gridControl1.MainView = lView;
+            ResetLayoutView(lView);
+            lView.CardMinSize = cardSize;
+            LayoutViewColumn colFileName = lView.Columns.AddField("文件名");
+            LayoutViewColumn colFilePath = lView.Columns.AddField("文件路径");
+            LayoutViewColumn colPhoto = lView.Columns.AddField("图片文件");
+            LayoutViewField fieldFileName = colFileName.LayoutViewField;
+            LayoutViewField fieldFilePath = colFilePath.LayoutViewField;
+            LayoutViewField fieldPhoto = colPhoto.LayoutViewField;
+            colFileName.Visible = true;
+            fieldPhoto.Move(new LayoutItemDragController(fieldPhoto, fieldFileName, InsertLocation.After, LayoutType.Vertical));
+            //fieldFilePath.Move(new LayoutItemDragController(fieldFilePath, fieldPhoto, InsertLocation.After, LayoutType.Vertical));
+
+            RepositoryItemPictureEdit riPictureEdit = this.gridControl1.RepositoryItems.Add("PictureEdit") as RepositoryItemPictureEdit;
+            riPictureEdit.SizeMode = PictureSizeMode.Stretch;
+            colPhoto.ColumnEdit = riPictureEdit;
+
+            fieldFileName.TextVisible = false;
+            fieldFilePath.TextVisible = false;
+            fieldPhoto.Text = "  ";
+
+            colFileName.OptionsColumn.ReadOnly = true;
+            colFilePath.OptionsColumn.ReadOnly = true;
+            colPhoto.OptionsColumn.ReadOnly = true;
+            colFileName.Visible = false;
+            colFilePath.Visible = false;
+            lView.CardCaptionFormat = "{2}    第{0}幅，共{1}幅";
+        }
+
+        private void ResetLayoutView(LayoutView layoutView)
+        {
+            try
+            {
+                layoutView.OptionsCustomization.AllowFilter = false;
+                layoutView.OptionsCustomization.AllowSort = false;
+                LayoutViewOptionsView layoutViewOptionsView = (LayoutViewOptionsView)layoutView.OptionsView;
+                layoutViewOptionsView.ViewMode = LayoutViewMode.Carousel;
+                layoutViewOptionsView.ShowHeaderPanel = false;
+                layoutViewOptionsView.ShowCardExpandButton = false;
+                layoutViewOptionsView.ShowCardExpandButton = false;
+                layoutView.Columns.Clear();
+            }
+            catch { }
+        }
+
+        private DataTable CreatDatatableOfFiles( string[] filePathes,  Size  imageSize)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("文件名", Type.GetType("System.String")));
+            dt.Columns.Add(new DataColumn("文件路径", Type.GetType("System.String")));
+            dt.Columns.Add(new DataColumn("图片文件", typeof(Image)));
+            
+            foreach (string filePath in filePathes)
+            {
+                DataRow newRow;
+                newRow = dt.NewRow();
+                newRow["文件名"] = Path.GetFileNameWithoutExtension(filePath);
+                newRow["文件路径"] = filePath;
+                Image resImage = Image.FromFile(filePath);
+                Image resImage1 = resImage.GetThumbnailImage(imageSize.Width, imageSize.Height, null, IntPtr.Zero);
+                newRow["图片文件"] = resImage1;
+                dt.Rows.Add(newRow);
+            }
+            return dt;
+        }
+
+        private void btnStatusMap_Click(object sender, EventArgs e)
+        {
+            SetGridControlView(mainFloderPath + "\\现状图", cardSize);
+        }
+
+        private void btnPlanMap_Click(object sender, EventArgs e)
+        {
+            SetGridControlView(mainFloderPath + "\\规划图", cardSize);
+        }
+
+        private void btnAnalysisMap_Click(object sender, EventArgs e)
+        {
+            SetGridControlView(mainFloderPath + "\\分析图", cardSize);
+        }
+    }
+}
